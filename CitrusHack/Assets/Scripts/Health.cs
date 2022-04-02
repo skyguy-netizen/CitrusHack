c9583f8feab335e84a38ee17a14bb4b1f3f0dba0
oscargzman <oscargzman77@gmail.com>
2022-04-02 16:13:25 -0700

    updated health script and added enemy object

---
+++ CitrusHack/Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 5;
+
+    private int MAX_HEALTH = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CitrusHack/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    // IN PROGRESS (Health script needed)
+
+    // private int damage = 1;
+    // private void OnButtonPress(GameObject victim)
+    // {
+    //     if (victim.GetComponent<Health>() != null)
+    //     {
+    //         Health health = victim.GetComponent<Health>();
+    //         health.Damage(damage);
+    //     }
+    // }
+
+}
