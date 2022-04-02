83e320f07d9f56f91f9e5a42cc49479840ace657
Aarav Sane <aaravtennis@gmail.com>
2022-04-02 16:47:23 -0700

    health and energy bars

--- CitrusHack/Assets/Scripts/Health.cs
+++ CitrusHack/Assets/Scripts/Health.cs
@@ -7,15 +7,19 @@ public class Health : MonoBehaviour
     // [SerializeField] private int health = 5;
 
     // private int MAX_HEALTH = 5;
-    
+    private const int MAX_HEALTH = 5;
+    private int currentHealth;
+
     void Start()
     {
-        
+        currentHealth = 5;
     }
 
     
     void Update()
     {
-        
+        for (int i = 0; i < currentHealth; i++){
+            
+        }
     }
 }
