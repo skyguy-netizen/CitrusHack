c8bf6ef32d7cb9c0bcca760b19a300d7df9ebc08
AadilU <aadilu2002@gmail.com>
2022-04-02 19:46:58 -0700

    commit

--- CitrusHack/Assets/TypeText.cs
+++ CitrusHack/Assets/TypeText.cs
@@ -6,9 +6,11 @@ using TMPro;
 public class TypeText : MonoBehaviour
 {
     public TMP_Text text1;
+    public GameObject ButtonList;
     void Update() {
         if (Input.GetKeyDown("space")) {
             text1.text = "";
+            ButtonList.SetActive(true);
         }
     }
 }
